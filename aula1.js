4dc19cc6630a522f10da5121397d37e7d4cf22ee
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-06-07 15:25:49 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,1 +1,15 @@
-console.log("hello");
+alert(floar)
+
+/*string, int, float, bolean*/
+
+var nome = 'vamos la'
+
+var number = -9
+
+var floar = 100.55
+
+var sim = 'true'
+
+console.log()
+alert()
+getSelection()
