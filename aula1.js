9029704d4ac9ae67e07fd656aad81af516c25e81
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-06-28 22:34:02 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,8 +1,6 @@
-/*string, int, float, bolean*/
+//string, int, float, bolean*/
 
 var nome = 'peterson'
-
-/*
 
 var number = -9 */
 
@@ -10,20 +8,17 @@ var floar = 100.55
 
 var sim = 'true'
 
-/*console.log()
-alert()
-getSelection()*/
+//console.log()
+//alert()
+//getSelection()*/
 
 console.log(nome)
 console.log(number)
 
 console.log("meu nome é" ,nome, "e eu tenho" ,30, "anos")
 
-/*
-
-var numero = null (é proposital que é null que é igual a não tem valor)
-var numero2 = undestnet (que não tem valor  ou esqueceu de colocar o valor)
- */
+//var numero = null (é proposital que é null que é igual a não tem valor)
+//var numero2 = undestnet (que não tem valor  ou esqueceu de colocar o valor)
 
 var nota = prompt('digite a nota final:')
 
@@ -41,7 +36,6 @@ if ( '3' === 3){
     documet.write( 'falso')
 }
 
-
 var nota = 10
 var media = 7
 
@@ -57,7 +51,12 @@ if ( nota >= media && 3 == 2 ){
     document.write('Reprovado')
  }
 
-
+//abaixo faz a troca de sim para não
+if (!(3 == 2( ){
+    document.write('Aprovado')
+ } else {
+    document.write('Reprovado')
+ }
 
 //CASTING
 //para somar string para 
