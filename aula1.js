bd842782fd7acb0d63ffc56f684957b83ce599bc
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-08-15 23:21:43 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -102,3 +102,14 @@ var não = 10
 var total = Quadro(sim,não);
 
 alert('resultado foi' + total);
+
+function Quadro(valor1,valor2){
+    return 10 + valor1 + valor2;
+}
+
+var sim = 50;
+var nao = 40;
+
+var total = Quadro(sim,nao);
+
+document.getElementById("teste").innerHTML= total;
