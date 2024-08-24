bd58eb232a670af019e32c09ab193d558fa70756
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-08-24 14:14:32 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -8,3 +8,17 @@ function Clicar() {
         document.body.style.color = "black";
     }
 }
+
+function Clicar() {
+    const mudar = getComputedStyle(document.body).background;
+
+    if (mudar.includes("linear-gradient")) {
+        // Muda para cor sólida
+        document.body.style.background = "#111111";
+        document.body.style.color = "white";
+    } else {
+        // Muda para o gradiente
+        document.body.style.background = "linear-gradient(to bottom, #b9b9b9, #d4e9e8, #d4e9e8, #d4e9e8, #d69074)";
+        document.body.style.color = "black";
+    }
+}
